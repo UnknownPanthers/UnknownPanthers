ab9271eb34ca437275257afaf4391ae8267b5a32
CashewRose <danieagnoletti@gmail.com>
2018-05-01 15:23:10 -0500

    Columns fixed for human viewing (minus the text content being offensive for a little one's eyes)

--- src/scripts/bootstrap.js
+++ src/scripts/bootstrap.js
@@ -1,17 +1,17 @@
 const container = document.querySelector(".container");
 
+const divTop = document.createElement("div");
+divTop.classList = "row"
 const build = (card) => {
-    const divTop = document.createElement("div");
-    const div = document.createElement("div");
     const divCard = document.createElement("div");
-    divTop.classList = "row"
+    const div = document.createElement("div");
     div.classList = "col-4";
     div.textContent = "look at me! I'm a div!";
     divCard.textContent = card;
+    divCard.classList = "default"
     div.appendChild(divCard)
     divTop.appendChild(div)
     container.appendChild(divTop)
-    // container.appendChild(div2)
 }
 
 build("fake");
