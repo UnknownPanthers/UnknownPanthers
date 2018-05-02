fb17d752e015b9e9f2a57a2e0d678f6c3f3425aa
CashewRose <danieagnoletti@gmail.com>
2018-05-02 11:36:23 -0500

    Basic drag and drop established (Booyah)

---
+++ src/scripts/dragDrop.js
@@ -0,0 +1,6 @@
+
+
+
+// <div id="div1" ondrop="drop(event)" ondragover="allowDrop(event)"></div>
+// <br>
+// <img id="drag1" src="img_logo.gif" draggable="true" ondragstart="drag(event)" width="336" height="69">
