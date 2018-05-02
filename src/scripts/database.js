cdb9e434d203717e1daf5eb354844e98e48257d4
binauralbeat <binauralbeat001@gmail.com>
2018-05-02 11:30:57 -0500

    form data to storage

--- src/scripts/database.js
+++ src/scripts/database.js
@@ -1,18 +1,18 @@
-const taskDatabase = {}
+// const taskDatabase = {}
 
-taskDatabase.cards = []
+// taskDatabase.cards = []
 
 
-// Add cards to the task database
-taskDatabase.cards.push({
-            name: "Jacob Smith",
-            description:"Yellow",
-            Start:"",
-            End:"",
-            Category:[],
-            Status:"",
-            Archive:false
-        })
+// // Add cards to the task database
+// taskDatabase.cards.push({
+//             name: "Jacob Smith",
+//             description:"Yellow",
+//             Start:"",
+//             End:"",
+//             Category:[],
+//             Status:"",
+//             Archive:false
+//         })
 
-localStorage.setItem(
-    taskDatabase,JSON.stringify(taskDatabase))
+// localStorage.setItem(
+//     taskDatabase,JSON.stringify(taskDatabase))
