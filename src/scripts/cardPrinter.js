950d93e1b8b6d03c9c9d2e3817514a52a0fb6a41
binauralbeat <binauralbeat001@gmail.com>
2018-05-03 15:37:59 -0500

    sweet panther animation

---
+++ src/scripts/cardPrinter.js
@@ -0,0 +1,10 @@
+// const build = require(./buildCard)
+
+// const tasks = require(./storeTask)
+
+// //
+// // let category = localStorage.getItem("taskFactory.category")
+// // let description = localStorage.getItem("taskFactory.description")
+// // let dueDate = localStorage.getItem("taskFactory.dueDate")
+
+//buildCard(taskDatabase)
